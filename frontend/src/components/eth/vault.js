ac0b2f67e2afb2a23d24e50dc18677f9f2b262a4
ihsan younes <ihsan.younes@wegift.io>
2021-11-03 10:10:50 +0100

    Working vault.deposit from UI

--- frontend/src/components/eth/vault.js
+++ frontend/src/components/eth/vault.js
@@ -40,17 +40,43 @@ export function TotalSupply(vault) {
     }).catch((err) => {
         console.log(err);
     }) 
-  }, [decimals])
-  return parseFloat(formatUnits(result, decimals)).toPrecision(4)
+  }, [vault, decimals])
+  return Math.round(parseFloat(formatUnits(result, decimals)) * 100) / 100
+}
+
+export function BalanceOf(vault) {
+  const {account, library, chainId} = useWeb3React()
+  const [result, setResult] = useState(0)
+  const [decimals, setDecimals] = useState(0)
+  useEffect(() => {
+    if (!vault){
+      return;
+    }
+    vault.decimals()
+    .then((r) => {setDecimals(r)})
+    .catch((err) => {console.log(err)});
+
+    vault.balanceOf(account)
+    .then((r) => {
+      setResult(r.toString());
+    }).catch((err) => {
+        console.log(err);
+    }) 
+  }, [vault, decimals, account])
+  return Math.round(parseFloat(formatUnits(result, decimals)) * 100) / 100
 }
 
 
 export async function Deposit(vault, val1, val2) {
   const accounts = await window.ethereum.request({ method: 'eth_accounts' });
   
-  vault.deposit(val1.toString(), val2.toString(), 0, 0, accounts[0]).then((r) => {
+  return vault.deposit(val1.toString(), val2.toString(), 0, 0, accounts[0]).then((r) => {
     //setResult(r.toString());
      console.log(r);
+     return r.wait();
+  }).then((r) => {
+    console.log("confirmed");
+    console.log(r);
   }).catch((err) => {
       console.log(err);
   }) 
